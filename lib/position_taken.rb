ba5d7d0e244445366311bb23830bf667f5d62a77
stephenchou-ww <stephen.chou@wework.com>
2018-02-19 20:38:14 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,10 @@
 # code your #position_taken? method here!
 
-def position_taken(input)
-
-
+def position_taken(board, index)
+  if board[index] == " "
+    return FALSE
+  else board[index] != " " && board[index] != NIL
+    return TRUE
+  end
+end
 
